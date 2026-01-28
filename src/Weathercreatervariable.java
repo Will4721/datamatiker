6c4164396946e9f20a094269f1b357869c6266ec
Will4721 <williamlehd@gmail.com>
2026-01-28 21:23:00 +0100

    Wtf

---
+++ src/Weathercreatervariable.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Weathercreatervariable {
+    Scanner myObj = new Scanner(System.in);
+   String[] weather = {"Thunder", "Rain","Dry", "Snow"};
+   int p = 0;
+   int temp = 0;
+   int windspeed = 0;
+   boolean rain = false;
+   System.out.println("would you like to start the weather\t type start to begin");
+
+   String start = myObj.nextLine();
+   for (i=0, i<10, i++){
+
+    }
+
+   while (p>0) {
+       System.out.println("hi")
+
+    }
+
+
+}
