814f7d7ce37d0af34a77720111a9d1c86a9a2756
Will4721 <williamlehd@gmail.com>
2026-01-29 12:19:01 +0100

    29-01-2026

--- src/Weathercreatervariable.java
+++ src/Weathercreatervariable.java
@@ -12,32 +12,36 @@ public class Weathercreatervariable {
 
         if (start.equals("start")){
             while (p == 0) {
+                if (type<50 && type>26){
+                    System.out.println("the weather is now thunder\n**************************");
+                }
+                if (type<25 && type>0){
+                    System.out.println("the weather is now dry\n**************************");
+                }
+                if (type<75 && type>51){
+                    System.out.println("the weather is now snow\n**************************");
+                }
+                if (type<100 && type>76){
+                    System.out.println("the weather is now rain\n**************************");
+                }
+                temp =  (int)(Math.random() * 101);
+                windspeed = (int)(Math.random() * 101);
+                type = (int)(Math.random() * 101);
                 Scanner newObj = new Scanner(System.in);
-                System.out.println("for diffrent weather type 'next'");
-                System.out.println("at any point type 'end' to stop program");
+                System.out.println("\n"+"for diffrent weather type 'next'");
+                System.out.println("at any point type 'end' to stop program :)");
                 String next = newObj.nextLine();
                 if (next.equals("next")){
                     temp =  (int)(Math.random() * 101);
                     windspeed = (int)(Math.random() * 101);
                     type = (int)(Math.random() * 101);
-                    System.out.println("the temp is now\t" + temp + "\t degree Celsius");
+                    System.out.println("**************************\nthe temp is now\t" + temp + "\t degree Celsius");
                     System.out.println("windspeed is now\t" + windspeed + "km/h");
                 }
                 if (next.equals("end")){
                     p = 1;
                 }
-                if (type<50 && type>26){
-                    System.out.println("the weather is now thunder");
-                }
-                if (type<25 && type>0){
-                    System.out.println("the weather is now dry");
-                }
-                if (type<75 && type>51){
-                    System.out.println("the weather is now snow");
-                }
-                if (type<100 && type>76){
-                    System.out.println("the weather is now rain");
-                }
+
             }
 
 
