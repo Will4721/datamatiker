cc6f4fb6e5d6f109f37b5a3620dab111cc61d47f
Will4721 <williamlehd@gmail.com>
2026-01-29 10:19:27 +0100

    example array

---
+++ src/array.java
@@ -0,0 +1,12 @@
+public static void main(String[] args) {
+
+    String[] LoanItem = {
+            "Inception", "Løvernes konge", "Star Wars", "Harry potter",
+            "Percy Jackson", "Hunger games", "Twilligt",
+            "Game of thrones", "Jurassic park novel"
+    };
+
+    for (int i = 0; i < LoanItem.length; i++) {
+        System.out.println(LoanItem[i]);
+    }
+}
