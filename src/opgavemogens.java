bb0c4e90517e4ce6822c2949bcab42a91b7f38bf
Will4721 <williamlehd@gmail.com>
2026-01-29 13:01:27 +0100

    opgave færdig

--- src/opgavemogens.java
+++ src/opgavemogens.java
@@ -1,5 +1,19 @@
 public class opgavemogens {
     public static void main(String[] args) {
         System.out.println("Mission\t Year\t Days in space\nIriss\t 2015\t 10\nHuginn\t 2023\t 180");
+
+        System.out.println("   /\\ \n  /  \\ \n /____\\ \n |    |\n |ISS |\n |____|\n   ||\n   ||\n  /  \\ \n /____\\ ");
+        //rocket effect
+        for (int i=0; i<12; i++){
+            System.out.println("  ||||");
+            //wait function taget fra stack overflow
+            try {
+                Thread.sleep(600);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+        }
+        System.out.println("**************************\n**************************\n    LAUNCH SUCCSESS\n    MISSION COMPLETE\n**************************\n**************************");
     }
 }
